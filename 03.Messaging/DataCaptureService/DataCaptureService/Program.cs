4d4f1e4885196f0ab7c6d4227619873b4a63fc51
Alex Puchok <a.puchek.90@gmail.com>
2022-06-24 22:48:18 +0300

    init

---
+++ 03.Messaging/DataCaptureService/DataCaptureService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataCaptureService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from capture!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03.Messaging/DataCaptureService/DataProcessingService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataProcessingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from processing!");
+            Console.ReadLine();
+        }
+    }
+}

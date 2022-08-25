3ad08ca6444c0b0597eabe20ff341f43521964e0
Alex Puchok <a.puchek.90@gmail.com>
2022-08-25 22:02:05 +0300

    add adapter task implementation

--- 08.GOF Cutting Shape/GOF Cutting Shape/Adapter/Program.cs
+++ 08.GOF Cutting Shape/GOF Cutting Shape/Adapter/Program.cs
@@ -1,1 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var foodElements = new Elements<string>(new[] { "apple", "banana", "watermelon" });
+var adaptedElements = new AdaptedElements<string>(foodElements);
+var printer = new Printer();
+printer.Print(adaptedElements);
+
+public class Printer
+{
+    public void Print<T>(IContainer<T> container)
+    {
+
+        foreach (var item in container.Items)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
+
+
+public interface IContainer<T>
+
+{
+    IEnumerable<T> Items { get; }
+
+    int Count { get; }
+}
+
+public interface IElements<T>
+
+{
+    IEnumerable<T> GetElements();
+}
+
+public class Elements<T> : IElements<T>
+{
+    private readonly IEnumerable<T> _collection;
+
+    public Elements(IEnumerable<T> collection)
+    {
+        _collection = collection ?? throw new ArgumentNullException(nameof(collection));
+    }
+
+    public IEnumerable<T> GetElements()
+    {
+        return this._collection;
+    }
+}
+
+public class AdaptedElements<T> : IContainer<T>
+{
+    private readonly IElements<T> _elements;
+
+    public AdaptedElements(IElements<T> elements)
+    {
+        _elements = elements ?? throw new ArgumentNullException(nameof(elements));
+    }
+
+    public IEnumerable<T> Items => this._elements.GetElements();
+
+    public int Count => this.Items.Count();
+}

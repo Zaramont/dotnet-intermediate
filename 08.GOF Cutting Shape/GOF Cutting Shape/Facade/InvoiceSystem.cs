1a08deae95741068a8009beb94851ec74cda8cf4
Alex Puchok <a.puchek.90@gmail.com>
2022-08-28 19:27:38 +0300

    add Facade task implementation

---
+++ 08.GOF Cutting Shape/GOF Cutting Shape/Facade/InvoiceSystem.cs
@@ -0,0 +1,15 @@
+﻿public class InvoiceSystem : IInvoiceSystem
+{
+    public void SendInvoice(Invoice invoice)
+    {
+        string message = invoice.ToString();
+        Console.WriteLine(message);
+    }
+}
+
+public interface IInvoiceSystem
+{
+    void SendInvoice(Invoice invoice);
+}
+
+public record Invoice(string productName, int quantity, string email, double price, string orderId);
---
+++ 08.GOF Cutting Shape/GOF Cutting Shape/Facade/PaymentSystem.cs
@@ -0,0 +1,14 @@
+﻿public class PaymentSystem : IPaymentSystem
+{
+    public bool MakePayment(Payment payment)
+    {
+        return true;
+    }
+}
+
+public interface IPaymentSystem
+{
+    bool MakePayment(Payment payment);
+}
+
+public record Payment(string orderId, double price);
